9e5a73772e46df2e1e3b147e2786d699f4669995
HashanEranga <ccmrhe@gmail.com>
2023-01-07 00:32:43 +0530

    csharp - Created sealed classes

---
+++ OOP/01-Classes/typesofclasses/Sealed/Mobile.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace OOP.Classes.typesofclasses.Sealed
+{
+    public sealed class Mobile
+    {
+        public int nextId = 0;
+        public int modelId;
+        public string? ModelName { get; set; }
+        public string? ModelDescription { get; set; }
+
+        public Mobile(string modelName, string modelDescription)
+        {
+            modelId = ++nextId;
+            ModelName = modelName;
+            ModelDescription = modelDescription;
+        }
+
+        public override string ToString()
+        {
+            return $"Model Id : {modelId}, Model Name : {ModelName}, Model Description : {ModelDescription}";
+        }
+    }
+}
+
