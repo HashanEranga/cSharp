de4ce9f0ab1220745c6b061c2beb912bec9aa813
HashanEranga <ccmrhe@gmail.com>
2023-01-06 18:39:38 +0530

    Created Static classes

---
+++ OOP/01-Classes/typesofclasses/Static/MathUtil.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace OOP.Classes.typesofclasses.Static
+{
+    public class MathUtil
+    {
+        public static double PI = 3.142;
+
+        public static double CalculateCircleArea(double radius)
+        {
+            return PI * radius * radius;
+        }
+    }
+}
+
