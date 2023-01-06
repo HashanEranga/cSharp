279f78ae2df518b05d336ec34b2674115d6defe9
HashanEranga <ccmrhe@gmail.com>
2023-01-06 18:06:44 +0530

    csharp - created concrete classes example

---
+++ OOP/01-Classes/typesofclasses/Concrete/Person.cs
@@ -0,0 +1,59 @@
+﻿using System;
+namespace OOP.Classes.typesofclasses.Concrete
+{
+    public class Person
+    {
+        private static int nextId = 0;
+        private readonly int id;
+        private string? firstName;
+        private string? lastName;
+        private int age;
+
+        public string FirstName
+        {
+            get
+            {
+                if (firstName != null) return firstName;
+                else return "";
+            }
+            set
+            {
+                if (value != null) firstName = value;
+            }
+        }
+
+        public string LastName
+        {
+            get
+            {
+                if (lastName != null) return lastName;
+                else return "";
+            }
+            set
+            {
+                if (value != null) lastName = value;
+            }
+        }
+
+        public int Age
+        {
+            get => age;
+            set => age = value;
+        }
+
+        public Person(string _fname, string _lname, int _age)
+        {
+            FirstName = _fname;
+            LastName = _lname;
+            Age = _age;
+
+            id = ++nextId;
+        }
+
+        public override string ToString()
+        {
+            return $"Id : {id}, Name : {FirstName} {LastName}, Age : {Age}";
+        }
+    }
+}
+
