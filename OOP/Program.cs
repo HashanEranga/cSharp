7463296bc0408193e727ca29b0ecc8f6d43c5f7f
HashanEranga <ccmrhe@gmail.com>
2023-01-06 18:06:21 +0530

    csharp - created initial broilerplate code

---
+++ OOP/Program.cs
@@ -0,0 +1,22 @@
+﻿using OOP.Classes.typesofclasses.Concrete;
+
+// using concrete classes
+Person hashan = new("Hashan", "Eranga", 25);
+Person ishini = new("Ishini", "Amasha", 21);
+Console.WriteLine(hashan.ToString());
+Console.WriteLine(ishini.ToString());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Console.ReadLine();
