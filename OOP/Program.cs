6b896affe6325fcca8449664faaf1f4b072cfaf3
HashanEranga <ccmrhe@gmail.com>
2023-01-06 18:39:58 +0530

    csharp - created demo for static classes

--- OOP/Program.cs
+++ OOP/Program.cs
@@ -1,4 +1,5 @@
 ﻿using OOP.Classes.typesofclasses.Concrete;
+using OOP.Classes.typesofclasses.Static;
 
 // using concrete classes
 Person hashan = new("Hashan", "Eranga", 25);
@@ -6,8 +7,8 @@ Person ishini = new("Ishini", "Amasha", 21);
 Console.WriteLine(hashan.ToString());
 Console.WriteLine(ishini.ToString());
 
-
-
+double answer = MathUtil.CalculateCircleArea(12.9);
+Console.WriteLine($" Answer for the area of the circle {answer}");
 
 
 
