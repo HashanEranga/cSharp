a59ffed591cf6ee3aaae8861e309585f50f6f7be
HashanEPerera <45037796+HashanEranga@users.noreply.github.com>
2023-01-07 13:56:59 +0530

    created demos for interfaces and abstract classes

--- OOP/Program.cs
+++ OOP/Program.cs
@@ -3,6 +3,8 @@ using OOP.Classes.typesofclasses.Concrete;
 using OOP.Classes.typesofclasses.Sealed;
 using OOP.Classes.typesofclasses.Static;
 using OOP.Encapsulation;
+using OOP.Polymorphism.ArchiverHandling;
+using OOP.Polymorphism.UserHandling;
 
 // using concrete classes
 Person hashan = new("Hashan", "Eranga", 25);
@@ -10,9 +12,17 @@ Person ishini = new("Ishini", "Amasha", 21);
 Console.WriteLine(hashan.ToString());
 Console.WriteLine(ishini.ToString());
 
+Console.WriteLine("");
+Console.WriteLine("==================");
+Console.WriteLine("");
+
 // static class demo 
 double answer = MathUtil.CalculateCircleArea(12.9);
-Console.WriteLine($" Answer for the area of the circle {answer}");
+Console.WriteLine($"Answer for the area of the circle {answer}");
+
+Console.WriteLine("");
+Console.WriteLine("==================");
+Console.WriteLine("");
 
 // abstract class demo
 MasterProduct masterItem_1 = new MasterProduct("MasterItem01", 100, true);
@@ -20,11 +30,19 @@ MasterProduct masterItem_2 = new MasterProduct("MasterItem02", 200, true);
 Console.WriteLine(masterItem_1.ToString());
 Console.WriteLine(masterItem_2.ToString());
 
+Console.WriteLine("");
+Console.WriteLine("==================");
+Console.WriteLine("");
+
 // using sealed class demo
 Mobile Nokia = new("Nokia", "Nokia Description");
 Mobile Samsung = new("Samsung", "Samsung Description");
 Console.WriteLine(Nokia.ToString());
 Console.WriteLine(Samsung.ToString());
+
+Console.WriteLine("");
+Console.WriteLine("==================");
+Console.WriteLine("");
 
 // using encapsulation in c#
 Student stuHashan = new("Hashan", "Eranga", 25, 23, 3.1);
@@ -32,5 +50,32 @@ Student stuIshini= new("Ishini", "Amasha", 21, 54, 3.6);
 Console.WriteLine(stuHashan.ToString());
 Console.WriteLine(stuIshini.ToString());
 
+Console.WriteLine("");
+Console.WriteLine("==================");
+Console.WriteLine("");
+
+// polymorphism abstract classes
+User contentManager = new ContentManager();
+User admin = new AdminUser();
+
+contentManager.ValidateAccessRights();
+admin.ValidateAccessRights();
+
+Console.WriteLine("");
+Console.WriteLine("==================");
+Console.WriteLine("");
+
+contentManager.getProfileInformation(new UserProfile());
+
+// Polimorphism Interfaces
+IArchiver zipArchiver = new ZipArchiver();
+IArchiver rarArchiver = new RarArchiver();
+
+zipArchiver.ArchiveFiles();
+rarArchiver.ArchiveFiles();
+
+Console.WriteLine("");
+Console.WriteLine("==================");
+Console.WriteLine("");
 
 Console.ReadLine();
