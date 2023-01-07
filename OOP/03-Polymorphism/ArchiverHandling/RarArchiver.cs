ccb0f0af9d451aa67fd2e0d516f8440c59986b52
HashanEPerera <45037796+HashanEranga@users.noreply.github.com>
2023-01-07 13:56:05 +0530

    Created interfaces for Archivers

---
+++ OOP/03-Polymorphism/ArchiverHandling/RarArchiver.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP.Polymorphism.ArchiverHandling
+{
+    public class RarArchiver : IArchiver
+    {
+        public void ArchiveFiles()
+        {
+            Console.WriteLine("Rar Archive created");
+        }
+    }
+}
---
+++ OOP/03-Polymorphism/ArchiverHandling/ZipArchiver.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP.Polymorphism.ArchiverHandling
+{
+    public class ZipArchiver : IArchiver
+    {
+        public void ArchiveFiles()
+        {
+            Console.WriteLine("Zip Archive created");
+        }
+    }
+}
