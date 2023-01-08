52cdd0e81d8324c8289ba0926f67e6c844692207
HashanEPerera <45037796+HashanEranga@users.noreply.github.com>
2023-01-09 00:25:27 +0530

    Created Polymorphism example

---
+++ OOP/03-Polymorphism/ProductHandling/Phone.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP.Polymorphism.ProductHandling
+{
+    public class Phone : ProductItem
+    {
+        public Phone(string _name) : base(_name)
+        {
+            Console.WriteLine("Phone Constructor called!");
+        }
+
+        public void Ring()
+        {
+            Console.WriteLine("Mobile Phone Rings!");
+        }
+
+        public override ProductItem[] ListVariants()
+        {
+            return new ProductItem[4];
+        }
+
+        public int CalculateAmountOfVariants()
+        {
+            return base.ListVariants().Count();
+        }
+    }
+}
---
+++ OOP/03-Polymorphism/ProductHandling/ProductItem.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP.Polymorphism.ProductHandling
+{
+    public class ProductItem
+    {
+        private int nextId = 0;
+        private int Id;
+
+        public string? Name { get; set; }
+
+        public int CalculateRemainingAmount()
+        {
+            return 100;
+        }
+
+        public virtual ProductItem[] ListVariants()
+        {
+            return new ProductItem[3];
+        }
+
+        public ProductItem(string _name)
+        {
+            Name = _name;
+            Id = ++nextId;
+        }
+    }
+}
